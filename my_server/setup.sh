20954af80d351fc8aae594a260075c115fec3a8e
Dany <azminfantik@gmail.com>
2021-05-08 10:01:20 +0300

    wp add

--- my_server/setup.sh
+++ my_server/setup.sh
@@ -1,18 +1,22 @@
 #prepare
 eval $(minikube docker-env);
 minikube addons enable metallb
-bash delete_all_images.sh
 
 #nginx
 kubectl delete svc nginx-svc
 kubectl delete deployment nginx-deployment
-docker build -t nginx nginx/
-kubectl apply -f nginx/configmap.yaml
-kubectl apply -f nginx/nginx.yaml;
+docker build -t nginx_image srcs/nginx/
+kubectl apply -f srcs/nginx/configmap.yaml
+kubectl apply -f srcs/nginx/nginx.yaml;
 
 #mysql
 kubectl delete deployment mysql-deployment
 kubectl delete svc mysql-svc
-docker build -t mysql mysql/
-kubectl apply -f mysql/mysql-deployment.yaml
+docker build -t mysql_image srcs/mysql/
+kubectl apply -f srcs/mysql/mysql-deployment.yaml
 
+#wordpress
+kubectl delete deployment wp-deployment
+kubectl delete svc wp-svc
+docker build -t wp_image srcs/wordpress
+kubectl apply -f srcs/wordpress/wordpress.yaml
--- my_server/srcs/mysql/mysql.sh
+++ my_server/srcs/mysql/mysql.sh
@@ -4,4 +4,5 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 mysqld & sleep 5
 
 mysql < /wordpress.sql
+
 supervisord
